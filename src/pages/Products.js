8fa477e76999f01db24ab8f4049946da0548fc63
amine aqebli <aqebli36@gmail.com>
2022-11-09 01:08:05 +0100

    switch

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,11 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Products = () => {
   return (
     <section>
       <h1>Products Page</h1>
       <ul>
-        <li>RTX 3060ti</li>
-        <li>RTX 3070ti</li>
-        <li>RTX 3090ti</li>
+        <li>
+          <Link to="/products/p1">RTX 3060ti</Link>
+        </li>
+        <li>
+          <Link to="/products/p2">RTX 3070ti</Link>
+        </li>
+        <li>
+          <Link to="/products/p3">RTX 3090ti</Link>
+        </li>
       </ul>
     </section>
   );
